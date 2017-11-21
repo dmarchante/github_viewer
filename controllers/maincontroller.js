96630e33886535af2bd9f8ac35bcbc1d0c01c65c
David Marchante <cdmarchante@gmail.com>
2017-11-21 16:45:42 -0500

    complete viewer to get ready to implement services

--- controllers/maincontroller.js
+++ controllers/maincontroller.js
@@ -1,5 +1,5 @@
 angular.module('myApp')
-    .controller('MainCtrl', function($scope, $http) {
+    .controller('MainCtrl', ['$scope', '$http', '$interval', '$log', '$anchorScroll', '$location', ($scope, $http, $interval, $log, $anchorScroll, $location) => {
 
         const onUserComplete = function(response) {
             $scope.user = response.data;
@@ -9,18 +9,39 @@ angular.module('myApp')
 
         const onRepos = (response) => {
             $scope.repos = response.data;
+            $location.hash("userDetails");
+            $anchorScroll();
         };
 
         const onError = (reason) => {
             $scope.error = "Could not fetch the data.";
         };
 
+        /*const decrementCountdown = () => {
+            $scope.countdown -= 1;
+            if ($scope.countdown < 1) {
+                $scope.search($scope.username);
+            }
+        };
+
+        let countdownInterval = null;
+        const startCountdown = () => {
+            coutndownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+        }*/
+
         $scope.search = (username) => {
+            $log.info("Searching for " + username);
             $http.get("https://api.github.com/users/" + username)
                 .then(onUserComplete, onError);
+            /*if (countdownInterval) {
+                $interval.cancel(countdownInterval);
+                $scope.countdown = null;
+            }*/
         };
 
         $scope.username = "";
         $scope.message = "GitHub Viewer";
         $scope.repoSortOrder = "-stargazers_count";
-    });
+        //$scope.countdown = 10;
+        //startCountdown();
+    }]);
