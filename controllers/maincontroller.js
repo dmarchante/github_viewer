0304c1f84a3333c890bb06b35f9851496a9936c0
David Marchante <cdmarchante@gmail.com>
2017-11-21 11:34:52 -0500

    updated to latest JS features (arrow functions, const, let, etc)

--- controllers/maincontroller.js
+++ controllers/maincontroller.js
@@ -1,21 +1,21 @@
 angular.module('myApp')
     .controller('MainCtrl', function($scope, $http) {
 
-        var onUserComplete = function(response) {
+        const onUserComplete = function(response) {
             $scope.user = response.data;
             $http.get($scope.user.repos_url)
                 .then(onRepos, onError);
         };
 
-        var onRepos = function(response) {
+        const onRepos = (response) => {
             $scope.repos = response.data;
         };
 
-        var onError = function(reason) {
+        const onError = (reason) => {
             $scope.error = "Could not fetch the data.";
         };
 
-        $scope.search = function(username) {
+        $scope.search = (username) => {
             $http.get("https://api.github.com/users/" + username)
                 .then(onUserComplete, onError);
         };
--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,3 @@
-var myApp = angular.module('myApp', []);
+{
+    const myApp = angular.module('myApp', []);
+}
